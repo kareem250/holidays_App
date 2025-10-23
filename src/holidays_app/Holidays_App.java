67e5c12c41038938e4e43be7243552145f462d0b
ABDO <bxdgamer10@gmail.com>
2025-10-24 00:32:50 +0300

    17

--- src/holidays_app/Holidays_App.java
+++ src/holidays_app/Holidays_App.java
@@ -14,24 +14,12 @@ public class Holidays_App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Kareem Ahmed Salama");
         System.out.println("Adel Reda Adel");
         System.out.println("Abd El Rahman");
         System.out.println("Ziad Mohamed Khalaf");
         System.out.println("ناقص خمسة كمان");
-=======
-        System.out.println("Hi");
-        System.out.println("iam adel");
-        System.out.println("ABDO");
-        System.out.println("عبدو حمار");
-        System.out.println("يالا يا ابن الحلال");
-        System.out.println("عدل انت بقى");
-        System.out.println("elzoz ya am .. habiby yaa am");
-        System.out.println("هو هو الزوز ياعم");
-        System.out.println("العادل يعم");
-        System.out.println("سسبةننتالبيس");
->>>>>>> 95b5b9748a034281122507e6dff2b50c53652f88
     }
     
 }
